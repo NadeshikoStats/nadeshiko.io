0e9d1b0881dd7c08cce987c2c894153ce5474053
Brooke Gill <96643991+brooke-gill@users.noreply.github.com>
2024-03-18 01:05:38 -0700

    Added dynamic page loading Page now imports content from JSON

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+app.get('/player/:name', (req, res) => {
+    const name = req.params.name;
+    res.render('player', { name });
+  });
+  
+  app.listen(port, () => {
+    console.log(`Listening at https://localhost:${port} !`);
+  });
