0f0a982d224cbf08b896145c6ed6d9d0800798e0
Brooke Gill <96643991+brooke-gill@users.noreply.github.com>
2024-03-25 00:05:00 -0600

    Created the basics for backend-frontend communication

---
+++ public/external/minecrafttext.js
@@ -0,0 +1,65 @@
+function generateMinecraftText(minecraftText) {
+    const regex = /[^§&]*[^§&]|[§&][0-9a-z][^§&]*/g;
+    const brokenUpStrings = minecraftText.match(regex) || [];
+    let returnString = "";
+  
+    brokenUpStrings.forEach(individual => {
+      let ending = '';
+      const code = individual.split(/[&§][0-9a-z]/);
+      const prefixMatch = individual.match(/[&§][0-9a-z]/);
+      const prefix = prefixMatch ? prefixMatch[0] : null;
+  
+      if (prefix) {
+        const actualCode = prefix[1];
+        const validColors = [`0`,`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`,`a`,`b`,`c`,`d`,`e`,`f`];
+  
+        if (validColors.includes(actualCode)) {
+          returnString += `<span class="m${actualCode}">`;
+          ending += "</span>";
+        } else {
+          switch(actualCode) {
+            case "l":
+              if(individual.length>2) {
+                returnString += '<span style="font-weight:bold;">';
+                ending = "</span>" + ending;
+              }
+              break;
+            case "m":
+              if(individual.length>2) {
+                returnString += '<strike>';
+                ending = "</strike>" + ending;
+              }
+              break;
+            case "n":
+              if(individual.length>2) {
+                returnString += '<span style="text-decoration: underline;">';
+                ending = "</span>" + ending;
+              }
+              break;
+            case "o":
+              if(individual.length>2) {
+                returnString += '<i>';
+                ending = "</i>" + ending;
+              }
+              break;
+            case "r":
+              returnString += ending;
+              ending = '';
+              break;
+          }
+        }
+        
+        if (code[1]) {
+          returnString += code[1];
+          if(ending && individual.length > 2) {
+            returnString += ending;
+            ending = '';
+          }
+        }
+      } else {
+        returnString += individual;
+      }
+    });
+  
+    return returnString;
+  }
